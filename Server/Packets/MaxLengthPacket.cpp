447e5315ee42d66bd627f69f40deca8e884451b2
Jason Monk <jmonk@dream-crushed.com>
2012-06-27 15:23:41 -0400

    missing file

---
+++ Server/Packets/MaxLengthPacket.cpp
@@ -0,0 +1,25 @@
+/*
+ * MaxLengthPacket.cpp
+ *
+ *  Created on: Jun 26, 2012
+ *      Author: jmonk
+ */
+
+#include "MaxLengthPacket.h"
+
+namespace dvs {
+
+MaxLengthPacket::MaxLengthPacket(unsigned char* data) :
+				Packet(data) {
+	Device* device = getDevice();
+
+	if (device != NULL) {
+		device->setMaxLength(data[4]);
+	}
+}
+
+MaxLengthPacket::~MaxLengthPacket() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dvs */
